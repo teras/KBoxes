a798b0a2830f9430857cb0b9464cacb3495120c6
Panayotis Katsaloulis <panayotis@panayotis.com>
2015-08-30 10:36:59 +0300

    Initial transfer to GitHub

---
+++ kboxes/kboxes.h
@@ -0,0 +1,136 @@
+/***************************************************************************
+                          kboxes.h  -  description
+                             -------------------
+    begin                : Πεμ Ιαν 20 17:36:13 EET 2000
+    copyright            : (C) 2000 by Panos Katsaloulis
+    email                : teras@writeme.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KBOXES_H
+#define KBOXES_H
+
+// include files for QT
+#include <qpopupmenu.h>
+#include <qmainwindow.h>
+#include <qmenubar.h>
+#include <qpixmap.h>
+#include <qapp.h>
+#include <qmsgbox.h>
+#include <qpushbutton.h>
+#include <qbuttongroup.h>
+
+// application specific includes
+#include "resource.h"
+#include <iostream>
+#include <string>
+#include <cmath>
+
+/**
+  * This Class is the base class for your application. It sets up the main
+  * window and providing a menubar, toolbar
+  * and statusbar. For the main view, an instance of class KBoxesView is
+  * created which creates your view.
+  */
+class KBoxes : public QMainWindow
+{
+  Q_OBJECT
+  
+  public:
+    /** construtor */
+    KBoxes();
+    /** destructor */
+    ~KBoxes();
+
+  public slots:
+  
+    /** Start a new game */
+    void slotGameNew();
+    /** Undos the current move */
+    void slotGameUndo();
+    /** exits the application */
+    void slotGameExit();
+
+    /** set the level to a new nomber */
+    void slotSkillChanged(int);
+
+    /** Set program for one player*/
+    void slotOptionsOne();
+    /** Set program for two players*/
+    void slotOptionsTwo();
+    /** Set set who plays first - computer or user*/
+    void slotOptionsFirst();
+
+    /** shows an about dlg*/
+    void slotHelpAbout();
+    /** shows a how to play dlg*/
+    void slotHelpHowto();
+	/** Every button click comes through here. */	
+	void slotPlayButtons(int);
+
+  private:
+
+
+	enum player_type { PLAYER, COMPUTER } ;
+
+    /** file_menu contains all items of the menubar entry "File" */
+    QPopupMenu *gameMenu;
+    /** edit_menu contains all items of the menubar entry "Skill" */
+    QPopupMenu *skillMenu;
+    /** view_menu contains all items of the menubar entry "View" */
+    QPopupMenu *optionsMenu;
+    /** view_menu contains all items of the menubar entry "Help" */
+    QPopupMenu *helpMenu;
+	QPushButton * buttons[16];
+	QButtonGroup * group;
+
+    /** initMenuBar creates the menu_bar and inserts the menuitems */
+    void initMenuBar();
+    /** initButtons creates the various buttons on the game-space */
+    void initButtons();
+	/** Initialize the system data. This function is called only once, in the beginning */
+	void initFirstTime();
+	/** Prepare the program for a new Game. */
+	void initGame ();
+	/** The computer does its move */
+	void playComputer();
+	/** Display an error message */
+	void error () const;
+	/** Wipe some boxes  */
+	void wipeOut(int, int, player_type);
+	/** come here when the player looses */
+	void playerLooses();
+	/** Come here when the player wins. */
+	void playerWins();
+
+	/** How smart the program should be */
+	int skill;
+	/** Number of players */
+	int no_of_players;
+	/** Set whether or not computer plays first */
+	bool computer_plays_first;
+
+	/** Store the number of the last selected box. Become -1 if none is selected. */
+	int clicked;
+	/** Artficial "inteligence" level data */
+	string level[17];
+	/** General map of boxes */
+	char map[17];	// last positions stores the null character;
+	/** Map of free areas. Contains 2 characters per entry.
+	First character is the position of the first box and second of the last.
+	There is a difference between this version and the VB one. In the VB version we store
+	as the second character the lenght, here we store the position. One more: in VB we store characters in
+	hexadecimal, here we care only of the char-'0' value. */
+	string freeMap;
+
+};
+#endif 
+
---
+++ kboxes/resource.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+                          resource.h  -  description
+                             -------------------
+    begin                : Πεμ Ιαν 20 17:36:13 EET 2000
+    copyright            : (C) 2000 by Panos Katsaloulis
+    email                : teras@writeme.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+
+///////////////////////////////////////////////////////////////////
+// resource.h  -- contains macros used for commands
+
+#define ID_BUTTON_1		1
+#define ID_BUTTON_2		1
+#define ID_BUTTON_3		1
+#define ID_BUTTON_4		1
+#define ID_BUTTON_5		1
+#define ID_BUTTON_6		1
+#define ID_BUTTON_7		1
+#define ID_BUTTON_8		1
+#define ID_BUTTON_9		1
+#define ID_BUTTON_10		1
+
+
+///////////////////////////////////////////////////////////////////
+// General application values
+#define IDS_APP_ABOUT               "         KBoxes!\nVersion " VERSION \
+                                    "\n(c) 1994-2000 by Panos Katsaloulis" \
+									"\n\n Please send bugs and reports to teras@writeme.com"
+
+#define IDS_APP_HOWTO				"            * HOW TO PLAY *\n\n" \
+								"   The purpose of this game is to wipe out \n" \
+								"horizontal lines of boxes, but not the last \n" \
+								"one. You can wipe out as mane or as little \n" \
+								"boxes as yo want, as long as they are in the\n" \
+								"same horizontal line and there is not any box in\n" \
+								"between which is alerady wiped out. However, you\n" \
+								"can wipe out any box (or boxes), no matter if \n" \
+								"they are in the beginning or in the middle of the\n" \
+								"physical line. The person who wipes out the last\n" \
+								"box is the looser.\n" \
+								"   To play this game you have to select at the first\n" \
+								"box you want to wipe out and then at the last box. If \n" \
+								"you want to wipe out only one box, click at it again.\n"
+/* \
+								"The face to the right shows the condition of the\n" \
+								"computer. If it smiles, it is almost certain that you\n" \
+								"are goining to loose this game..."
+*/
+#define IDS_STATUS_DEFAULT          "Ready."
+
+#endif // RESOURCE_H
